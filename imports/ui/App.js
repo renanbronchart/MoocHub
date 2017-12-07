ac2d2b4ac0f80bfbaf7ddf5f9c37edec0410d403
Renan Bronchart <renan.bronchart@gmail.com>
2017-12-07 21:41:57 +0100

    [Feat:Account] Add dropdown account signIn with password

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+// import component to login and sign up
+import AccountsUIWrapper from './components/AccountsUiWrapper.js';
+
 // App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
       <div className="container">
+        <AccountsUIWrapper />
         <header>
           <h1>Mooc Hub</h1>
         </header>
