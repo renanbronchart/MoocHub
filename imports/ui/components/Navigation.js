a3ca030e10157855e4a5f97be5439793747afcd5
renanbronchart <renan.bronchart@gmail.com>
2017-12-08 21:38:09 +0100

    [Fix:Nav] Add style on nav

--- imports/ui/components/Navigation.js
+++ imports/ui/components/Navigation.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Icon } from 'antd';
 
 export const Navigation = () => (
-  <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/courses">Courses</Link></li>
-  </ul>
+  <Menu
+    defaultSelectedKeys={['1']}
+    mode="inline"
+    theme="dark"
+    className='navigation'
+  >
+    <Menu.Item key="1">
+      <Link to="/">Accueil</Link>
+    </Menu.Item>
+    <Menu.Item key="2">
+      <Link to="/courses">Liste des cours</Link>
+    </Menu.Item>
+    <Menu.Item key="3">
+      <Icon type="inbox" />
+      <span>Option 3</span>
+    </Menu.Item>
+  </Menu>
 )
